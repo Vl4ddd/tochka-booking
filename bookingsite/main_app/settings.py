25d3c7dac26f8f86ae4c371cbdf363d26c108d1d
Vladislav Ostanin <ostanin8900@yandex.ru>
2024-05-16 00:15:46 +0500

    поменял навзания

--- bookingsite/main_app/settings.py
+++ bookingsite/main_app/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'bookingsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '<dbname>',
+        'USER': 'postgres',
+        'PASSWORD': '<password>',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
