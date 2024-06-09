987c4006e4f05626b9cfc537a866cad5333c7d65
Vladislav Ostanin <ostanin8900@yandex.ru>
2024-06-09 20:04:31 +0500

    layout+index

--- bookingsite/hotel_app/views.py
+++ bookingsite/hotel_app/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'hotel_app/index.html',{'name':'VLad'})
