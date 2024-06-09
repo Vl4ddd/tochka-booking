902e3e72bd44a32aa7bea3d6e6ffcdb94fcc2e33
Vladislav Ostanin <ostanin8900@yandex.ru>
2024-06-09 23:03:30 +0500

    формы и регистрация

--- bookingsite/hotel_app/models.py
+++ bookingsite/hotel_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Hotel(models.Model):
+    hotel_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    stars = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+class Room(models.Model):
+    room_id = models.AutoField(primary_key=True)
+    hotel_id = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    room_count = models.IntegerField()
+    price = models.IntegerField()
 
 
+
+class Booking(models.Model):
+    booking_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
+    hotel_id = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    checkin_date = models.DateField()
+    checkout_date = models.DateField()
+
+  
