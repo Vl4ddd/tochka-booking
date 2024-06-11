470397204f4800167ce1d9d521058036222b1dca
Vladislav Ostanin <ostanin8900@yandex.ru>
2024-06-12 03:20:45 +0500

    (

--- bookingsite/hotel_app/forms.py
+++ bookingsite/hotel_app/forms.py
@@ -1,2 +1,33 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
+from .models import Booking
+
+import datetime
+
+
+class BookingDate(forms.ModelForm):
+
+    checkin_date = forms.DateField(
+        widget=forms.DateInput(attrs={"class": "form-control", "type": "date"}),
+        required=True,
+    )
+    checkout_date = forms.DateField(
+        widget=forms.DateInput(attrs={"class": "form-control", "type": "date"}),
+        required=True,
+    )
+
+    class Meta:
+        model = Booking
+        fields = ["checkin_date", "checkout_date"]
+
+        def clean_renewal_date(self):
+
+            cleaned_data = super().clean()
+            checkin_date = cleaned_data.get("checkin_date")
+            checkout_date = cleaned_data.get("checkout_date")
+            if checkin_date < datetime.date.today() or checkin_date > checkout_date:
+                raise ValidationError("Выберите правильную дату")
+            return cleaned_data
+
+           
--- bookingsite/hotel_app/views.py
+++ bookingsite/hotel_app/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .models import Hotel, Room
+
+from .models import Hotel, Room, Booking
+from .forms import BookingDate
+
+import datetime
 
 
 def home(request):
@@ -20,4 +24,41 @@ def hotel(request, post_id):
         "hotel_id": post.hotel_id,
     }
 
-    return render(request, "hotel_app/post.html", context)
+    return render(request, "hotel_app/hotel.html", context)
+
+
+@login_required
+def booking(request, post_id):
+    try:
+        booking = Booking.objects.filter(room_id=post_id)
+    except:
+        booking = None
+    
+
+    room = get_object_or_404(Room, pk=post_id)
+
+    if request.method == "POST":
+        form = BookingDate(request.POST)
+
+        if form.is_valid():
+            form.save(commit=False)
+            Booking.user_id = request.user.id
+            Booking.checkin_date = form.cleaned_data["checkin_date"]
+            Booking.checkout_date = form.cleaned_data["checkout_date"]
+            Booking.hotel_id = Room.objects.get(pk=post_id).hotel_id
+            Booking.room_id = Room.objects.get(pk=post_id)
+
+            form.save()
+            return redirect("/")
+    else:
+        form = BookingDate()
+
+    context = {
+        "booking": booking,
+        "room": room,
+        "room_id": room.room_id,
+        "data": datetime.date,
+        "form": form,
+    }
+
+    return render(request, "hotel_app/booking.html", context)
